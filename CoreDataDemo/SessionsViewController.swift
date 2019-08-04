84cf2839411b4314d0b0e80e0ab591b40b342652
Zev Eisenberg <zev@zeveisenberg.com>
2019-08-04 19:03:02 -0400

    Don't allow deletion. It is left as an exercise to the reader.

--- CoreDataDemo/SessionsViewController.swift
+++ CoreDataDemo/SessionsViewController.swift
@@ -36,17 +36,4 @@ class SessionsViewController: UITableViewController {
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
-    }
-
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }   
-    }
-
-
 }
