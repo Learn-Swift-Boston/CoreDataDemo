fd8a227ef35fafa67f49c7f0bbf7ebb8a854e373
Matt Dias <matt.dias6@gmail.com>
2019-08-04 16:30:12 -0400

    Initial Commit

---
+++ CoreData/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  CoreData
+//
+//  Created by Matthew Dias on 8/4/19.
+//  Copyright © 2019 Matthew Dias. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CoreData/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  CoreData
+//
+//  Created by Matthew Dias on 8/4/19.
+//  Copyright © 2019 Matthew Dias. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
